68d08e1ce76a609e1aeb331f0e991c3cb75689ed
Mark Massoud <mark.massoud@unity3d.com>
2022-02-08 16:57:03 -0500

    test matrix

--- src/setupTests.js
+++ src/setupTests.js
@@ -7,14 +7,42 @@ import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 import '@testing-library/jest-dom';
 
 export const formElCount = +process.env.FORM_EL_COUNT || 1000;
-console.log(`Running with formElCount=${formElCount}`);
 
 Enzyme.configure({ adapter: new Adapter() });
 
+const timers = {};
+
+const TEST_IT_SLOW_THRESHOLD_MS = 1000;
+const TEST_FILE_SLOW_THRESHOLD_MS = 5000;
+
 beforeEach(() => {
-  console.time(expect.getState().currentTestName);
+  const testName = expect.getState().currentTestName;
+  timers[testName] = window.performance.now();
 });
 
 afterEach(() => {
-  console.timeEnd(expect.getState().currentTestName);
+  const testName = expect.getState().currentTestName;
+  const time = Math.round(window.performance.now() - timers[testName]);
+
+  if (time > TEST_IT_SLOW_THRESHOLD_MS) {
+    console.warn(`Test assertion ${testName} is taking more than ${TEST_IT_SLOW_THRESHOLD_MS}ms to run, consider optimizing it (${time}ms)`);
+  }
+
+  delete timers[testName];
+});
+
+beforeAll(() => {
+  const testPath = expect.getState().testPath;
+  timers[testPath] = window.performance.now();
+});
+
+afterAll(() => {
+  const testPath = expect.getState().testPath;
+  const time = Math.round(window.performance.now() - timers[testPath]);
+
+  if (time > TEST_FILE_SLOW_THRESHOLD_MS) {
+    console.warn(`Test file ${testPath} is taking more than ${TEST_FILE_SLOW_THRESHOLD_MS}ms to run, consider optimizing it (${time}ms)`);
+  }
+
+  delete timers[testPath];
 });
